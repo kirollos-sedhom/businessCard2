981663fe234ac2ff55c1e7d179d7a7cdc3056276
carlo <kirollosedhom@gmail.com>
2024-07-06 13:12:39 +0300

    fix vite

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@
 export default function Profile(){
     return (
         <div className="profile">
-            <img src="/src/assets/carol2.jpg" alt="most beautiful girl"/>
+            <img src="https://scontent.fcai26-1.fna.fbcdn.net/v/t39.30808-6/366971963_836741661177697_1288316172624570933_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=XiA74IoD2iUQ7kNvgERyWwg&_nc_ht=scontent.fcai26-1.fna&oh=00_AYBXg1Zs2D5Zx9drs0SrG_20Xk7h4cvJfxOG_AXL-FkFkA&oe=668ED868" alt="most beautiful girl"/>
             <h1>Carol Aziz</h1>
             <p>Highschool graduate</p>
             <p><a href="#">carolaziz.website</a></p>
